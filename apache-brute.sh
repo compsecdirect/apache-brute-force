95f5c7a404d0e93fe49232ee85c988ab72c7048c
compsecdirect <jfer@compsecdirect.com>
2017-05-31 05:48:46 -0400

    Added whitelist and usage

--- apache-brute.sh
+++ apache-brute.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Created by @jfersec
-# Version 0.1
+# Version 0.2
 # Decription: This script will
 # 1. parse and generate a list of top 20 visitors from apache logs
 # 2. Run geoiplookup to identify country
@@ -9,9 +9,12 @@
 # 5. If actions are over 100 for each ip, they become blocked.
 # 6. It also runs whois to show you who you are about to block.
 # Todo: Parse admin email from whois and send emails to abuse contact, once, with relevant informatino to support claim.
-   
+# Pre-usage: set your ip whitelist via "ip\|ip2\|ip3"
+# Usage: ./apache-brute.sh
+WHITELIST="ip1\|ip2"
+
 echo "Top Visitors by Country"
-zgrep -v dummy /var/log/apache2/access.* | awk -F ":\|\ -" '{print $2}' 2>/dev/null | sort | uniq -c | sort -rn | head -20 | awk -F " " '{print $2}' > top20.txt; while read line; do echo $line `geoiplookup $line`; done < top20.txt
+zgrep -v dummy /var/log/apache2/access.* | grep -v "$WHITELIST" | awk -F ":\|\ -" '{print $2}' 2>/dev/null | sort | uniq -c | sort -rn | head -20 | awk -F " " '{print $2}' > top20.txt; while read line; do echo $line `geoiplookup $line`; done < top20.txt
 echo "Top Brute-Forces by IP"
 while read line; do
         counter=$(zgrep $line /var/log/apache2/access.* | grep "301\|404" | wc -l)
